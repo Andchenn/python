ffb3c5ed2a7503470e6bdee8a72e4427ed4ea6f7
Andchen <1476204794@qq.com>
2018-07-05 19:29:49 +0800

    迭代

---
+++ com/Counter.py
@@ -0,0 +1,15 @@
+class Counter(object):
+    def __init__(self, low, high):
+        self.current = low
+        self.high = high
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        # 返回下一个值直到当值大于high
+        if self.current > self.high:
+            raise StopIteration
+        else:
+            self.current += 1
+            return self.current - 1
