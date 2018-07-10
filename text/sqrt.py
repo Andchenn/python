fc7af3575b1094a3117a59a529e121042fe4a944
Andchen <1476204794@qq.com>
2018-07-10 19:03:54 +0800

    平方根

---
+++ text/sqrt.py
@@ -0,0 +1,7 @@
+num = int(input('请输入一个数字：'))
+num_sprt = num ** 0.5
+print('%d平方根为%d' % (num, num_sprt))
+
+num = float(input('请输入一个数字：'))
+num_sprt = num ** 0.5
+print('%.2f平方根为%.2f' % (num, num_sprt))
